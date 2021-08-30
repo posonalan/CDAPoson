4860b48c222f808b91e6941210f092133b67ca6b
posonalan <65591159+posonalan@users.noreply.github.com>
2021-08-30 16:51:55 +0200

    ALAN ...

--- liste.php
+++ liste.php
@@ -6,5 +6,33 @@ $nomVille = (isset($_SESSION['nom'])) ? $_SESSION['nom'] : '';
 $numeroDepartement = (isset($_SESSION['numeroDepartement'])) ? $_SESSION['numeroDepartement'] : '';
 
 
+$affichage ='
 
-?>
+<div class="ville">
+            <div class="inter">
+                <img class="LogoVille" src="../IMAGE/logoLyon.png" alt="logo ville">
+                <div class="nomVille">
+                    <h4>' . $ville->getNomVille() . '</h4>
+                </div>
+                <div class="numVille">
+                    <h3>'. $ville->getNumeroVille() .'</h3>
+                </div>
+                <div class="codePostal">
+                    <h4>'. $ville->getCodePostal() .'</h4>
+                </div>
+                <div class="dateMAJ">
+                    <h4>'. $ville->getDateMiseAJour() .'</h4>
+                </div>
+                <input type="submit" value="modifier" />
+                <input type="submit" value="supprimer" />
+            </div>
+        </div>
+
+    
+       ';  
+    
+
+          echo $affichage;  
+
+
+       
